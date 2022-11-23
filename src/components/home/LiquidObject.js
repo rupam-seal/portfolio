185babbdccd19cfe769ea3e7510b4e04fb334da9
rupam-seal <rupam.x.seal@gmail.com>
2022-11-23 22:36:28 +0530

    add animation: liquid object

---
+++ src/components/home/LiquidObject.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LiquidObject = () => {
+  return (
+    <>
+      <div className="background-layer"></div>
+      <div className="liquid-object"></div>
+    </>
+  );
+};
+
+export default LiquidObject;
