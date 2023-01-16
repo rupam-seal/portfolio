bb42b2401ba466e832e526ac9a50b3e7ffa78ce5
rupam-seal <rupam.x.seal@gmail.com>
2023-01-16 22:04:36 +0530

    modified: index file

--- src/index.js
+++ src/index.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 // PAGES
-import Home from './pages/Home';
+import Home from './pages/HomePage';
+import Posts from './pages/PostsPage';
+import WorksPage from './pages/WorksPage';
+
+// COMPONENTS
 import Footer from './components/common/Footer';
 import Topbar from './components/common/Topbar';
+import Navigation from './components/common/Navigation';
+import LiquidObject from './components/common/LiquidObject';
+
 // CSS
 import './assets/css/Index.css';
 import './assets/css/common/Section.css';
 import './assets/css/home/Hero.css';
 import './assets/css/home/Skills.css';
-import './assets/css//home/Projects.css';
+import './assets/css//home/Works.css';
+import './assets/css//home/Card.css';
 import './assets/css/common/Footer.css';
 import './assets/css/common/Topbar.css';
 import './assets/css/common/Navigation.css';
 import './assets/css/common/Menu.css';
 import './assets/css/common/LiquidObject.css';
+
 // Colors
 import './assets/css/common/Colors.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Topbar />
-    {/* Home */}
-    <div className="body__container">
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <Topbar />
+      <div className="body__container">
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Home />} exact />
+          <Route path="/posts/:id" element={<Posts />} exact />
+          <Route path="/works/:id" element={<WorksPage />} exact />
+        </Routes>
+        {/* Liquid Object */}
+        <LiquidObject />
+        <Footer />
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
